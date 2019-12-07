194c87bd3515800d72d078294cd2d694db140dff
Chad McVeigh <cbmcveig@usc.edu>
2019-12-06 18:37:21 -0800

    done

--- mm.c
+++ mm.c
@@ -60,6 +60,7 @@ static void set_footer(BlockHeader *bp, int size, int allocated) {
     set_header((BlockHeader *)footer_addr, size, allocated);
 }
 
+
 /**
  * Find the payload starting address given the address of a block header.
  *
@@ -94,7 +95,7 @@ static BlockHeader *get_prev(BlockHeader *bp) {
  */
 static BlockHeader *get_next(BlockHeader *bp) {
     int this_size = get_size(bp);
-    char *next_addr = NULL;  // TODO: to implement, look at get_prev
+    char *next_addr = (char*)bp + this_size;  // TODO: to implement, look at get_prev
     return (BlockHeader *)next_addr;
 }
 
@@ -168,7 +169,25 @@ static BlockHeader *free_tailp;
  * @param bp address of the header of the block to add
  */
 static void free_list_prepend(BlockHeader *bp) {
-    // TODO: implement
+    //if free list is empty
+    if(free_headp == NULL){
+        free_headp = bp;
+        free_tailp = bp;
+        set_next_free(bp, NULL);
+        set_prev_free(bp, NULL);
+    }
+    else{
+
+        //if free list has 1 block
+        if(get_next_free(free_headp) == NULL && get_prev_free(free_tailp) == NULL){
+            set_prev_free(free_tailp, bp);
+        }
+
+        set_next_free(bp, free_headp);
+        set_prev_free(free_headp, bp);
+        free_headp = bp;
+        set_prev_free(free_headp, NULL);
+    }
 }
 
 /**
@@ -177,7 +196,24 @@ static void free_list_prepend(BlockHeader *bp) {
  * @param bp address of the header of the block to add
  */
 static void free_list_append(BlockHeader *bp) {
-    // TODO: implement
+    if(free_tailp == NULL){
+        free_headp = bp;
+        free_tailp = bp;
+        set_next_free(bp, NULL);
+        set_prev_free(bp,NULL);
+    }
+    else{
+
+        //if free list has 1 block
+        if(get_next_free(free_headp) == NULL && get_prev_free(free_tailp) == NULL){
+            set_next_free(free_headp, bp);
+        }
+
+        set_next_free(free_tailp, bp);
+        set_prev_free(bp, free_tailp);
+        free_tailp = bp;
+        set_next_free(free_tailp, NULL);
+    }
 }
 
 /**
@@ -186,7 +222,31 @@ static void free_list_append(BlockHeader *bp) {
  * @param bp address of the header of the block to remove
  */
 static void free_list_remove(BlockHeader *bp) {
-    // TODO: implement
+
+    //if free_list has one block
+    if(get_next_free(free_headp) == NULL && get_prev_free(free_headp) == NULL){
+        free_headp = NULL;
+        free_tailp = NULL;
+    }
+    
+    //if block to remove is the head
+    else if(free_headp == bp){
+        free_headp = get_next_free(free_headp);
+        set_prev_free(free_headp, NULL);
+    }
+
+    //if block to remove is tail
+    else if(free_tailp == bp){
+        free_tailp = get_prev_free(free_tailp);
+        set_next_free(free_tailp, NULL);
+    }
+
+    //if block to remove is in the middle
+    else{
+          set_next_free(get_prev_free(bp), get_next_free(bp));
+          set_prev_free(get_next_free(bp), get_prev_free(bp));
+    }
+    bp = NULL;
 }
 
 /**
@@ -198,32 +258,54 @@ static void free_list_remove(BlockHeader *bp) {
  */
 static BlockHeader *free_coalesce(BlockHeader *bp) {
 
+
     // mark block as free
     int size = get_size(bp);
     set_header(bp, size, 0);
     set_footer(bp, size, 0);
 
+    BlockHeader* next = get_next(bp);
+    BlockHeader* prev = get_prev(bp);
+
     // check whether contiguous blocks are allocated
     int prev_alloc = get_allocated(get_prev(bp));
     int next_alloc = get_allocated(get_next(bp));
 
-    if (prev_alloc && next_alloc) {
-        // TODO: add bp to free list
+    //AA
+     if (prev_alloc && next_alloc) {
+        free_list_append(bp);
+        return bp;
+    }
+
+    //AF
+    else if (prev_alloc && !next_alloc) {
+        free_list_remove(next);
+        free_list_append(bp);
+        set_header(bp, size + get_size(next), 0);
+        set_footer(bp, size + get_size(next), 0);
         return bp;
 
-    } else if (prev_alloc && !next_alloc) {
-        // TODO: remove next block from free list
-        // TODO: add bp to free list
-        // TODO: coalesce with next block
-        return bp;
+    }
 
-    } else if (!prev_alloc && next_alloc) {
-        // TODO: coalesce with previous block
+    //FA
+    else if (!prev_alloc && next_alloc) {
+
+        int m = get_size(prev);
+        set_header(prev, m + size, 0);
+        set_footer(prev, m + size, 0);
         return get_prev(bp);
+    } 
 
-    } else {
-        // TODO: remove next block from free list
-        // TODO: coalesce with previous and next block
+    //FF
+    else {
+        int n1 = get_size(prev);
+        int n2 = size;
+        int n3 = get_size(next);
+
+        set_header(prev, n1 + n2 + n3, 0);
+        set_footer(prev, n1 + n2 + n3, 0);
+
+        free_list_remove(next);
         return get_prev(bp);
     }
 }
@@ -272,11 +354,24 @@ int mm_init(void) {
     heap_blocks += 1;                   // point to the prologue header
 
     // TODO: extend heap with an initial heap size
+    BlockHeader *extend = extend_heap(1<<11);
+    if(extend == NULL){
+        return -1;
+    }
+    // TODO: extend heap with an initial heap size
     return 0;
 }
 
 void mm_free(void *bp) {
-    // TODO: move back 4 bytes to find the block header, then free block
+ 
+    BlockHeader* bp_addr = (BlockHeader*) bp;
+    BlockHeader* bp_head = bp_addr -1;
+
+    if(get_allocated(bp_head) == 0)
+        return;
+    
+    free_coalesce(bp_head);
+
 }
 
 /**
@@ -287,7 +382,16 @@ void mm_free(void *bp) {
  *         all smaller than `size`.
  */
 static BlockHeader *find_fit(int size) {
-    // TODO: implement
+    BlockHeader * bp = free_headp;
+
+    //going through free list to find block
+    while(bp != NULL){
+        if(get_size(bp) >= size){
+            return bp;
+        }
+        bp = get_next_free(bp);
+    }
+
     return NULL;
 }
 
@@ -299,6 +403,63 @@ static BlockHeader *find_fit(int size) {
  * @return pointer to the header of the allocated block
  */
 static BlockHeader *place(BlockHeader *bp, int size) {
+
+    //if bp is already allocated, then removing from free list
+    if(get_allocated(bp) == 0){
+        free_list_remove(bp);
+    }
+
+
+    int m = get_size(bp);
+    int n = size;
+
+    //if there is enough space to make an extra free block
+    if(m - n >= 16){
+
+
+        //if block size is large
+        if(n > 100){
+
+            //setting header and footer
+            set_header(bp, m - n, 0);
+            set_footer(bp, m - n, 0);
+
+            //creating a new free block
+            BlockHeader* next = get_next(bp);
+            set_header(next, n, 1);
+            set_footer(next, n, 1);
+
+            //adding block to list
+            free_list_append(bp);
+            return next;
+        }
+
+        //if a small block
+        else{
+
+            //set new allocated block to n size
+            set_header(bp, n, 1);
+            set_footer(bp, n, 1);
+
+            //creating a new free block
+            BlockHeader* next = get_next(bp);
+            set_header(next, m - n, 0);
+            set_footer(next, m - n, 0);
+
+            //adding block to list
+            free_list_append(next);
+            return bp;
+        }
+    }
+    else{
+        //giving the entire block
+        set_header(bp, m, 1);
+        set_footer(bp, m, 1);
+    }
+    return bp;
+
+
+
     // TODO: if current size is greater, use part and add rest to free list
     // TODO: return pointer to header of allocated block
     return NULL;
@@ -323,9 +484,17 @@ void *mm_malloc(size_t size) {
 
     int required_size = required_block_size(size);
 
-    // TODO: find a free block or extend heap
-    // TODO: allocate and return pointer to payload
-    return NULL;
+
+    BlockHeader* freeBlock = find_fit(required_size);
+
+    //no freeBlock, must extend heap
+    if(freeBlock == NULL){
+        freeBlock = extend_heap(required_size);
+    }
+
+    freeBlock = place(freeBlock, required_size);
+
+    return get_payload_addr(freeBlock);
 }
 
 void *mm_realloc(void *ptr, size_t size) {
@@ -340,9 +509,46 @@ void *mm_realloc(void *ptr, size_t size) {
         return NULL;
 
     } else {
-        // TODO: reallocate, reusing current block if possible
-        // TODO: copy data over new block with memcpy
-        // TODO: return pointer to payload of new block
-        return NULL;
+
+        //getting bp
+        BlockHeader* bp = ptr - 4;
+
+        //finding current block size
+        int m = get_size(bp);
+        int n = required_block_size(size);
+
+        if((m - n < 24) && (m - n >= 0)){
+            return ptr;
+        }      
+       
+       //reuse block
+       else if(m - n >= 24){
+            mm_malloc(size);
+            memcpy(get_payload_addr(get_next(bp)), ptr, n);
+            free_list_append(bp);
+            return get_payload_addr(get_next(bp));
+        }
+
+        else{
+            BlockHeader* free;
+
+            if(get_size(get_next(bp)) == 0){
+
+                if(n - m < 24){
+                    n = 24 + m;
+                    free = extend_heap(24);
+                }
+                else{
+                    free = extend_heap(n - m);
+                }
+
+                free_list_remove(free);
+
+                set_header(bp, n, 1);
+                set_footer(bp, n, 1);
+
+                return ptr;
+            }
+        }
     }
 }
